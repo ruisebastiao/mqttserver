251948f36df30d625fbec172181de3794927a468
Rui Sebastião <rsebastiao@keyplastics.com>
2017-03-02 22:57:46 +0000

    updated

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ var server = http.createServer(app);
 // };
 
 var server = new mosca.Server({
+
+    persistence: {
+        factory: mosca.persistence.Memory
+    }
     //backend: pubsubsettings,
     // persistence: {
     //     factory: mosca.persistence.Mongo,
@@ -20,6 +24,26 @@ var server = new mosca.Server({
     // }
 }, function() {
     server.attachHttpServer(app);
+});
+
+
+server.on('clientConnected', function(client) {
+    console.log('client connected', client.id);
+});
+
+
+server.on('clientDisconnected', function(client) {
+    count = 0;
+    console.log('client disconnect', client.id);
+});
+
+
+var count = 0;
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+    count++;
+    console.log(count + ':Published', packet.topic, packet.payload.toString());
 });
 
 server.on('ready', function() {
