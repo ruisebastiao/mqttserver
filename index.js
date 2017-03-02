ccacdee8794783b7327c0f239bbe9d3a0636a7e5
Rui Sebastiao <ruisebastiao@gmail.com>
2017-03-02 23:37:24 +0000

    updated

--- index.js
+++ index.js
@@ -27,9 +27,13 @@
 // });
 
 var mosca = require("mosca");
+var port= process.env.PORT;
+
+console.log(port);
+
 var broker = new mosca.Server({
     http: {
-        port: process.env.PORT || 5000,
+        port:port || 5000,
         bundle: true,
         static: './'
     }
